c8e448f349b1abda5997ab7d60df44ba189af566
Thiago <thisangon03@gmail.com>
2025-05-20 15:18:14 -0300

    começando arquivos de análise semântica

--- etapa4/semantic.cpp
+++ etapa4/semantic.cpp
@@ -1,3 +1,34 @@
 // Análise semântica - cpp
 // Thiago dos Santos Gonçalves - 00344587
 // Semestre 2025/1
+
+#include "semantic.hpp"
+
+int semanticErrors = 0;
+
+void semanticCheck(AST* nodo){
+    if (nodo==0) return;
+    switch(nodo->tipo){
+        case AST_SYMBOL:{
+            // verifica se o simbolo existe na tabela de simbolos
+            SYMBOL* s = symbolLookup((char*)nodo->simbolo->text.c_str());
+            if (s == 0){
+                printf("Erro semantico: simbolo %s nao declarado\n", nodo->simbolo->text.c_str());
+                semanticErrors++;
+            }
+            break;
+        }
+        case AST_DEF:{
+            // verifica se o simbolo ja existe na tabela de simbolos
+            SYMBOL* s = symbolLookup((char*)nodo->filho[0]->simbolo->text.c_str());
+            if (s != 0){
+                printf("Erro semantico: simbolo %s ja declarado\n", nodo->filho[0]->simbolo->text.c_str());
+                semanticErrors++;
+            }
+            break;
+        }
+    }
+    for (uint32_t i=0; i<nodo->filho.size(); i++){
+        semanticCheck(nodo->filho[i]);
+    }
+}
--- etapa4/semantic.hpp
+++ etapa4/semantic.hpp
@@ -2,8 +2,16 @@
 // Thiago dos Santos Gonçalves - 00344587
 // Semestre 2025/1
 
+#ifndef SEMANTIC_HPP
+#define SEMANTIC_HPP
 #include "ast.hpp"
 #include "symbols.hpp"
 #include <stdio.h>
 #include <cstring>
 #include <stdlib.h>
+
+extern int semanticErrors;
+
+void semanticCheck(AST* nodo);
+
+#endif
